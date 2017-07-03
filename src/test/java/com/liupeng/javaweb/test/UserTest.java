95ebe4f08594e3d8d7ce253b23b309201aadd4f2
liupeng <liupengfun@163.com>
2017-07-03 22:53:44 +0800

    databasehelper的uml

--- src/test/java/com/liupeng/javaweb/test/UserTest.java
+++ src/test/java/com/liupeng/javaweb/test/UserTest.java
@@ -7,12 +7,15 @@ import org.junit.Test;
  * Created by lenovo on 2017/7/3.
  */
 public class UserTest {
+    /**
+     * 单元测试前的初始化操作
+     */
     @Before
     public void init(){
         // TODO: 2017/7/3
     }
     @Test
     public void getListTest(){
-        // TODO: 2017/7/3  
+        // TODO: 2017/7/3
     }
 }
